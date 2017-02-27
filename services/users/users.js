75b29fe34d9e95939a9544bc8cb39b31b41431fa
Tyko Niemi <pullamozzo@gmail.com>
2017-02-27 09:24:22 +0200

    restify API

--- services/users/users.js
+++ services/users/users.js
@@ -6,12 +6,44 @@ import os from 'os'
 const log = bunyan.createLogger( { name: 'users', level: 'DEBUG' } )
 
 export function startServer() {
+  var PATH ='/users'
   // Create a restify server
   const server = restify.createServer()
+  // Set CORS
   server.use( restify.CORS() )
-  // Define the endpoint
-  server.get( '/users/:userid', (req, res, next) => {
-    res.header('Host', os.hostname() ) // For scaling demonstration
+  //Set parser
+  server.use(restify.bodyParser({ mapParams: true}))
+  // Endpoint for getting user information
+  server.get({path : PATH + '/:userid', version : '0.0.1'}, getUser)
+  // Endpoint for user creation
+  server.post({path : PATH , version : '0.0.1'}, postNewUser)
+  server.listen( process.env.PORT || 8080, process.env.IP || "0.0.0.0", () =>
+    log.info( '%s listening at %s', server.name, server.url )
+ ) 
+}
+function postNewUser(req,res,next){
+  var newUser = {}
+  newUser.userid = req.params.userid
+  newUser.firstname = req.params.firstname
+  newUser.lastname = req.params.lastname
+  newUser.age = req.params.age
+  newUser.addresses = req.params.addresses
+  
+  res.setHeader('Access-Control-Allow-Origin','*')
+  
+  models.user.upsert(newUser, { include: models.address, logging: log.debug.bind(log) }, function(err, success){
+    console.log('Response success '+success)
+    console.log('Response error '+err)
+    if(success){
+      res.send(201, newUser)
+      return next()
+    }else{
+      return next(err)
+    }
+  })
+}
+function getUser(req, res, next){
+   res.header('Host', os.hostname() ) // For scaling demonstration
     models.user.findOne( { where: { userid: req.params.userid },
       include: models.address, logging: log.debug.bind(log) } ).then( (user) => {
         if ( user ) res.send(user)
@@ -19,8 +51,4 @@ export function startServer() {
           req.params.userid + ' not found'))
         next()
       })
-  })
-  server.listen( process.env.PORT || 8080, process.env.IP || "0.0.0.0", () =>
-    log.info( '%s listening at %s', server.name, server.url )
-  )
 }
